245ad3cac7c5f2703387d0c8a667272eb6769653
Sean Downs <spdowns@protonmail.com>
2020-07-17 10:46:25 -0700

    add ui, not finished. thinking of an order class or

--- Bakery/Main.cs
+++ Bakery/Main.cs
@@ -1,12 +1,24 @@
 using System;
+using BreadLoaf;
+using Pastry;
 
-namespace project
+namespace Bakery
 {
-  public class project
+  public class OrderUI
   {
     public static void Main()
     {
-      
+      Console.WriteLine("Hello User; Welcome to the future of bread ordering!");
+      Console.WriteLine("We have two items for sale. Bread and Pastry.");
+      Console.WriteLine("How many bread do you want?");
+      string stringBreadloafs = Console.ReadLine();
+      Console.WriteLine("How many Pastry do you want?");
+      string stringPastryItem = Console.ReadLine();
+
+      int breadLoafs = int.Parse(stringBreadloafs);
+      int pastryItem = int.Parse(stringPastryItem);
+
+      Order
     }
   }
 }
