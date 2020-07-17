9ff82854e74f5ad21e65c55aa27cd56ec1105b9e
Sean Downs <spdowns@protonmail.com>
2020-07-17 13:13:09 -0700

    have trouble adding a method to class

--- Bakery.Tests/ModelsTests/BreadTests.cs
+++ Bakery.Tests/ModelsTests/BreadTests.cs
@@ -17,7 +17,7 @@ namespace BreadLoaf.Tests
     public void CalcLoafs_CalculatesTotalOFBraodLoafs_Total()
     {      
       Bread newOrder = new Bread(3);
-      newOrder.breadTotal();
+      newOrder.BreadLoafs.breadTotal();
       Assert.AreEqual(15, newOrder);
     }
   }
